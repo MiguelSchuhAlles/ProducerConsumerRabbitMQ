cf820b15309c1ce86da347bd09bdf8c85ccc413a
Miguel Schuh Alles <miguel.schuh@totvs.com.br>
2021-09-22 23:17:15 -0300

    first commit

---
+++ Producer/Sender.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Channels;
+using RabbitMQ.Client;
+using System.Text;
+using System.Net.Http;
+
+namespace Producer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                string message = "Getting started with .Net Core RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine($"Sent message '{message}'");
+
+                Console.WriteLine("Press any key to exit the Sender app...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
